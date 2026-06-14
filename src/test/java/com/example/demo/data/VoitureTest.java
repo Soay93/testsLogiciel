08f9d19255e5547e1bdd67c45b95adf82e1617f4
Soay93 <meslinfloriane@gmail.com>
2026-06-14 15:32:03 +0200

    Tests JUnit de Voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -1,17 +1,60 @@
 package com.example.demo.data;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-@SpringBootTest
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void creerVoitureAvecConstructeurVide() {
+        Voiture v = new Voiture();
+
+        assertNull(v.getMarque());
+        assertEquals(0, v.getPrix());
+        assertEquals(0, v.getId());
+    }
+
+    @Test
+    void creerVoitureAvecConstructeurParametre() {
+        Voiture v = new Voiture("Peugeot", 15000);
+
+        assertEquals("Peugeot", v.getMarque());
+        assertEquals(15000, v.getPrix());
+        assertEquals(0, v.getId());
+    }
+
+    @Test
+    void testerSetGetId() {
+        Voiture v = new Voiture();
+        v.setId(1);
+
+        assertEquals(1, v.getId());
+    }
+
+    @Test
+    void testerSetGetMarque() {
+        Voiture v = new Voiture();
+        v.setMarque("Renault");
+
+        assertEquals("Renault", v.getMarque());
+    }
+
+    @Test
+    void testerSetGetPrix() {
+        Voiture v = new Voiture();
+        v.setPrix(20000);
+
+        assertEquals(20000, v.getPrix());
+    }
+
+    @Test
+    void testerToString() {
+        Voiture v = new Voiture("BMW", 30000);
+        v.setId(2);
+
+        assertEquals("Car{marque='BMW', prix=30000, id=2}", v.toString());
     }
 
 }
